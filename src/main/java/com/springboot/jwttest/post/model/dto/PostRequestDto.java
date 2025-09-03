ba5dfc3bd0ac0137d65670714af026f86470ffa6
minsu47722 <minsu47722@naver.com>
2025-09-03 09:04:18 +0900

    7

--- src/main/java/com/springboot/jwttest/post/model/dto/PostRequestDto.java
+++ src/main/java/com/springboot/jwttest/post/model/dto/PostRequestDto.java
@@ -4,13 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class PostRequestDto {
-    // 테스트용 주석
-    //xptmxm 2
-    //3
-    //4
-    //5
-    //6
-    //7
     private String title;
     private String content;
 }
