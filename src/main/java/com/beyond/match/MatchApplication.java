20fe330b58a8feb20b5fae27b5f9761da7a67391
minsu47722 <minsu47722@naver.com>
2025-09-03 16:32:22 +0900

    수정

--- src/main/java/com/beyond/match/MatchApplication.java
+++ src/main/java/com/beyond/match/MatchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwttestApplication {
+public class MatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JwttestApplication.class, args);
+        SpringApplication.run(MatchApplication.class, args);
     }
 
 }
