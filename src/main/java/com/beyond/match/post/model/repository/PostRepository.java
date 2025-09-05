6ca85a0eb2c11ff76f6fa93b12f8dc9cd765a08d
minsu47722 <minsu47722@naver.com>
2025-09-05 09:50:26 +0900

    post 삭제

--- src/main/java/com/beyond/match/post/model/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.beyond.match.post.model.repository;
-
-import com.beyond.match.post.model.vo.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Integer> {
-}
