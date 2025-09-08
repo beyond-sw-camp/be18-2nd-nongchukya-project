76e128e7e595bd8359a07201284f2467e5663405
minsu47722 <minsu47722@naver.com>
2025-09-08 10:03:44 +0900

    댓글, 대댓글 기능 추가

--- src/main/java/com/beyond/match/community/comment/model/service/CommentService.java
+++ src/main/java/com/beyond/match/community/comment/model/service/CommentService.java
@@ -1,9 +1,22 @@
 package com.beyond.match.community.comment.model.service;
 
+import com.beyond.match.community.comment.model.dto.CommentRequestDto;
+import com.beyond.match.community.comment.model.dto.CommentResponseDto;
 import com.beyond.match.community.comment.model.vo.Comment;
 import com.beyond.match.community.post.model.vo.Post;
 import com.beyond.match.user.model.vo.User;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface CommentService {
     Comment save(User user, Post post, Comment parentComment, String content);
+
+    Comment getCommentById(int commentId);
+
+    List<CommentResponseDto> getCommentsByPostId(int postId);
+
+    CommentResponseDto updateComment(int commentId, CommentRequestDto commentRequestDto, User user);
+
+    void deleteComment(int commentId, User user);
 }
