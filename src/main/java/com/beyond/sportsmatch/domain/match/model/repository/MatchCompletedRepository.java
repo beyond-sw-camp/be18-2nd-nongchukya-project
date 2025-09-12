74758e1b5ae5fabc4ba2992be83724fa6e215cf6
jaesangE <gnwldzm@gmail.com>
2025-09-12 18:18:42 +0900

    repo -> repository ,SportRepository 위치 유저 repository로 리팩터링 기존 스포츠 Repository 삭제

--- src/main/java/com/beyond/sportsmatch/domain/match/model/repository/MatchCompletedRepository.java
+++ src/main/java/com/beyond/sportsmatch/domain/match/model/repository/MatchCompletedRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MatchCompletedRepo extends JpaRepository<MatchCompleted, Integer> {
+public interface MatchCompletedRepository extends JpaRepository<MatchCompleted, Integer> {
 }
